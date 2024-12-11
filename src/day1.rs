ca00162af436b11021e0971fb275f025e8c5820e
gcanat <gui.canat@gmail.com>
2024-12-11 23:49:45 +0100

    add test for day1

--- src/day1.rs
+++ src/day1.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use std::iter::zip;
 
 #[aoc_generator(day1)]
-pub fn gen_d1_p1(input: &str) -> (Vec<usize>, Vec<usize>) {
+pub fn parse(input: &str) -> (Vec<usize>, Vec<usize>) {
     let mut left_list: Vec<usize> = Vec::new();
     let mut right_list: Vec<usize> = Vec::new();
     let _ = input
@@ -18,7 +18,7 @@ pub fn gen_d1_p1(input: &str) -> (Vec<usize>, Vec<usize>) {
 }
 
 #[aoc(day1, part1)]
-pub fn solve_d1_p1(input: &(Vec<usize>, Vec<usize>)) -> usize {
+pub fn part1(input: &(Vec<usize>, Vec<usize>)) -> usize {
     let mut left_list = input.0.to_owned();
     left_list.sort_unstable();
     let mut right_list = input.1.to_owned();
@@ -29,7 +29,7 @@ pub fn solve_d1_p1(input: &(Vec<usize>, Vec<usize>)) -> usize {
 }
 
 #[aoc(day1, part2)]
-pub fn solve_d1_p2(input: &(Vec<usize>, Vec<usize>)) -> usize {
+pub fn part2(input: &(Vec<usize>, Vec<usize>)) -> usize {
     let lhs = input.0.clone();
     let rhs = input.1.clone();
 
@@ -39,14 +39,34 @@ pub fn solve_d1_p2(input: &(Vec<usize>, Vec<usize>)) -> usize {
         rhs_count.entry(*n).and_modify(|v| *v += 1).or_insert(1);
     }
 
-    let mut total = 0;
     // iterate on the left hand side list and compute the weighted sum
-    for n in lhs.iter() {
+    lhs.iter().fold(0, |acc, n| {
         if let Some(val) = rhs_count.get(n) {
-            total += n * val;
-            // remove entry so we only count it once
-            rhs_count.remove(n);
+            acc + (n * val)
+        } else {
+            acc
         }
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &'static str = "3   4\n\
+                                 4   3\n\
+                                 2   5\n\
+                                 1   3\n\
+                                 3   9\n\
+                                 3   3";
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(part1(&parse(INPUT)), 11);
     }
-    total
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(&parse(INPUT)), 31);
+    }
 }
